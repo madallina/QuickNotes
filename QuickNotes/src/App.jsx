f34fe5488b2f1d1ed32fd1567b5a7f6c1d76ff40
madallina <goncearucmadalina@yahoo.com>
2024-10-14 11:16:49 +0300

    Refactored app structure: split into 3 components

--- QuickNotes/src/App.jsx
+++ QuickNotes/src/App.jsx
@@ -1,22 +1,48 @@
 import {useState} from 'react'
+import { useEffect } from 'react';
+import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
 import './App.css'
 
 function App() {
 const [notes, setNotes]=useState([]);
+const [newNote, setNewNote]=useState('');
 
-const addNote=(newNote)=>{
-  setNotes([...notes,newNote]);
+useEffect(() => {
+  const savedNotes = localStorage.getItem('notes');
+  if (savedNotes) {
+    setNotes(JSON.parse(savedNotes));
+  }
+}, []);
+
+
+useEffect(() => {
+  localStorage.setItem('notes', JSON.stringify(notes));
+}, [notes]);
+
+
+const addNote = () => {
+  if (newNote.trim() !== '') {
+    setNotes([...notes, newNote]);
+    setNewNote(''); 
+  }
 };
 
-const deleteNote = (index)=>{
-  const updateNotes= notes.filter((note,ind)=>i!==index);
-  setNotes(updateNotes);
-}
+
+const deleteNote = (index) => {
+  const updatedNotes = notes.filter((note, i) => i !== index);
+  setNotes(updatedNotes);
+};
+
   return (
     <>
-      
+    <div>
+      <h1>My notes</h1>
+      <NoteForm newNote={newNote} setNewNote={setNewNote} addNote={addNote} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
